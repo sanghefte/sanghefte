e1fb0f7ffe6fda8e3a5a607d6ea12404b7f45a79
vemund <x16759109+Vemhas@users.noreply.github.com>
2022-02-04 11:34:28 +0100

    Firebase initialization and setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
 import "./Song.css";
-import { SangHefte } from "./SangHefte";
+import { SangHefte } from "./components/SangHefte";
+import {db} from "./util/firebase-config"
+import {collection, getDocs} from "firebase/firestore"
 
 function App() {
-  return <SangHefte />;
+const [data, setData] = useState([]);
+
+const dataCollectionRef = collection(db, "songs");
+
+useEffect(() => {
+  const getData = async() => {
+    const data = await getDocs(dataCollectionRef);
+    setData(data.docs.map((doc)=>({
+      ...doc.data(), id: doc.id,
+    })))
+  }
+  getData()
+  }, [])
+
+  return (
+      <div>
+        {
+          data.map(d =>{
+            console.log(data)
+            return(
+                <div>
+                  <h1>Name: {d.Name}</h1>
+                  <h1>Lyric: </h1>
+                  {d.lyrics}
+                </div>
+            )
+          } )
+        }
+        {/*<SangHefte/>*/}
+      </div>
+  );
 }
 
 export default App;
---
+++ src/util/firebase-config.ts
@@ -0,0 +1,16 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
