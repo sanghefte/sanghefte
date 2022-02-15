55b1fd7a23ca42cf9e9266b4ae179641c5a28468
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2022-02-15 14:15:35 +0100

    Fix unused import and missing key id in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
 import "./Song.css";
-import { SangHefte } from "./components/SangHefte";
 import {db} from "./util/firebase-config"
 import {collection, getDocs} from "firebase/firestore"
 
@@ -24,18 +23,16 @@ useEffect(() => {
   return (
       <div>
         {
-          data.map(d =>{
-            console.log(data)
+          data.map((d, index) =>{
             return(
-                <div>
-                  <h1>Name: {d.Name}</h1>
-                  <h1>Lyric: </h1>
+                <div key={index}>
+                  <h1 >Name: {d.Name}</h1>
+                  <h1 >Lyric: </h1>
                   {d.lyrics}
                 </div>
             )
           } )
         }
-        {/*<SangHefte/>*/}
       </div>
   );
 }
