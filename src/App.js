539ce7ce3d0e663375ee57e80f000c38a0177815
trymve <trymve@stud.ntnu.no>
2022-02-16 14:47:24 +0100

    Gjort det mulig å opprette et sanghefte som lagres i databasen under sanghefter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
 import "./Song.css";
 import {db} from "./util/firebase-config"
-import {collection, getDocs} from "firebase/firestore"
+import {collection, getDocs, setDoc, doc} from "firebase/firestore"
+import { ChakraProvider, Button, Input  } from '@chakra-ui/react'
 
 function App() {
 const [data, setData] = useState([]);
+const [sanghefteTitle, setSanghefteTitle] = useState();
 
+
+const sanghefterRef = collection(db, "sanghefter")
 const dataCollectionRef = collection(db, "songs");
 
 useEffect(() => {
@@ -20,20 +24,17 @@ useEffect(() => {
   getData()
   }, [])
 
+  const createNewSang = async () => {
+    await setDoc(doc(db, "sanghefter", sanghefteTitle), {
+    });
+
+  }
+
   return (
-      <div>
-        {
-          data.map((d, index) =>{
-            return(
-                <div key={index}>
-                  <h1 >Name: {d.Name}</h1>
-                  <h1 >Lyric: </h1>
-                  {d.lyrics}
-                </div>
-            )
-          } )
-        }
-      </div>
+    <ChakraProvider>
+      <Input placeholder={"Sanghefte tittel"} onChange={e => setSanghefteTitle(e.target.value)}></Input>
+      <Button onClick={createNewSang} >Test</Button>
+    </ChakraProvider>
   );
 }
 
