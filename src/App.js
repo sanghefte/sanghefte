1f03385163cb2c7b3e28a96f35665c6dd522ab4e
Henrik Giil Liisberg <henrigli@stud.ntnu.no>
2022-02-17 13:13:39 +0100

    Added landingpage and header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import "./Song.css";
 import { db } from "./util/firebase-config";
 import { collection, getDocs } from "firebase/firestore";
+import { ChakraProvider } from '@chakra-ui/react'
+import { LandingPage } from "./components/LandingPage"
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher"
 
 function App() {
   const [data, setData] = useState([]);
@@ -24,17 +27,9 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {data.map((d, index) => {
-        return (
-          <div key={index}>
-            <h1>Name: {d.Name}</h1>
-            <h1>Lyric: </h1>
-            {d.lyrics}
-          </div>
-        );
-      })}
-    </div>
+      <ChakraProvider>
+        <LandingPage />
+      </ChakraProvider>
   );
 }
 
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link, Flex, Box, Input, Container, VStack, Button, useColorModeValue } from "@chakra-ui/react";
+import { Header } from "./Header";
+
+export const LandingPage = () => {
+    const bgcolor = useColorModeValue("white", "whiteAlpha.50");
+  return (
+    <Box>
+      <Header />
+      <Flex
+        align="center"
+        justify={{ base: "center", md: "space-around", xl: "space-between" }}
+        direction={{ base: "column-reverse", md: "row" }}
+        wrap="nowrap"
+        minH="100vh"
+        px={8}
+        //bg="blue.800"
+      >
+        <Container>
+          <VStack>
+            <Box p={5} borderRadius="lg" shadow="md" bg={bgcolor}>
+              <Input size="lg" placeholder="Enter PIN" />
+              <Button
+                isFullWidth
+                mt={3}
+                //variant="outline"
+              >
+                Enter
+              </Button>
+            </Box>
+          </VStack>
+        </Container>
+      </Flex>
+    </Box>
+  );
+};
