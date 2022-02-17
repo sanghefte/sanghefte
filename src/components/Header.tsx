3f5bf4c51bcf2462a3a5d35db1b5ec01b351f8e7
Henrik Giil Liisberg <henrigli@stud.ntnu.no>
2022-02-17 13:13:39 +0100

    Added landingpage and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Heading, Flex, Spacer } from "@chakra-ui/react";
+import { Heading, Flex, Spacer, Box } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const Header = () => {
   return (
-    <Flex p={3} mr={3}>
-      <Spacer />
-      <ColorModeSwitcher />
+    <Flex p={3} mr={3} right="0" position="fixed">
+        <Box/>
+        <Spacer />
+        <Box p={3}>
+            <ColorModeSwitcher />
+        </Box>
+
     </Flex>
   );
 };
