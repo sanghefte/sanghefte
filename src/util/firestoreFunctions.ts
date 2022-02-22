97b18188a6c95af14b636d86621e65f54ffc20e1
trymve <trymve@stud.ntnu.no>
2022-02-22 19:20:33 +0100

    ADD: mulighet til å slette sanghefte, input felt for kodeord til sanghefte

--- src/util/firestoreFunctions.ts
+++ src/util/firestoreFunctions.ts
@@ -1,4 +1,4 @@
-import {collection, getDoc, getDocs, setDoc, doc} from "firebase/firestore";
+import {collection, deleteDoc, getDoc, getDocs, setDoc, doc} from "firebase/firestore";
 import {db} from "./firebase-config";
 
 export type Song = {
@@ -8,7 +8,7 @@ export type Song = {
 }
 
 const dataCollectionRef = collection(db, "songs");
-const docRef = doc(db, "sanghefter", "Update")
+
 
 export const getAllSongs = async (): Promise<Array<Song>> => {
     const snapshot = await getDocs(dataCollectionRef);
@@ -30,14 +30,19 @@ export const getAllSongs = async (): Promise<Array<Song>> => {
 Creates a new "sanghefte" in the sanghefte collection. path="sanghefter" id=2nd parameter" data afterwards.
 Checks if the document allready exist.
  */
-export const createSanghefte = async () => {
+export const createSanghefte = async (docName: string) => {
+    const docRef= doc(db, "sanghefter", docName)
     const docSnap = await getDoc(docRef)
     if(docSnap.exists()){
         console.log("JA DEN FINNES")
     }
     else{
-        setDoc(doc(db, "sanghefter", "Update"), {
-            title: "test4"
+        setDoc(doc(db, "sanghefter", docName), {
         });
     }
+}
+
+export const deleteSanghefte = async(docName: string) => {
+    const docRef= doc(db, "sanghefter", docName)
+    await deleteDoc(docRef);
 }
