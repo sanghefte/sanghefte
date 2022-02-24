fe30a0ec844fbe02665907d73a189dc86ef2d8cd
trymve <trymve@stud.ntnu.no>
2022-02-24 13:32:25 +0100

    ADD: mulighet for å opprette sang fra frontend siden

--- src/components/NewSong.tsx
+++ src/components/NewSong.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { useToast, Link, Flex, Box, Input, Container, VStack, Button, useColorModeValue, Heading, Textarea, Divider } from "@chakra-ui/react";
 import { Header } from "./Header";
+import React_2 from "framer-motion/dist/framer-motion";
+import {createSong} from "../util/firestoreFunctions";
 
 export const NewSong = () => {
     const toast = useToast() //ChakraUI funksjon for å få en bekreftelses-toast https://chakra-ui.com/docs/feedback/toast
+    const [title, setTitle] = useState("")
+    const [text, setText] = useState("")
+    const [artist, setArtist] = useState("")
+
+    const handleClick = () => {
+        toast({
+            title: 'Sang lagt til.',
+            description: "Sangen har blitt lagt til i sangheftet.",
+            status: 'success',
+            duration: 3000,
+            isClosable: true,
+        });
+        createSong("test", title, text, artist, "fintakt")
+    }
 
     return(
         <Flex
@@ -18,21 +34,13 @@ export const NewSong = () => {
                     <Heading>Legg til ny sang</Heading>
                     <Divider />
                     <Heading size="sm"> Tittel </Heading>
-                    <Input textAlign="center" placeholder="Legg til sangtittel"/>
+                    <Input textAlign="center" placeholder="Legg til sangtittel" onChange={e => setTitle(e.target.value)}/>
                     <Heading size="sm"> Artist </Heading>
-                    <Input textAlign="center" placeholder="Legg til artist"/>
+                    <Input textAlign="center" placeholder="Legg til artist" onChange={e => setArtist(e.target.value)}/>
                     <Heading size="sm"> Sangtekst </Heading>
-                    <Textarea textAlign="center" placeholder="Legg inn sangtekst" />
+                    <Textarea textAlign="center" placeholder="Legg inn sangtekst" onChange={e => setText(e.target.value)}/>
                     <Button
-                        onClick={() =>
-                            toast({
-                                title: 'Sang lagt til.',
-                                description: "Sangen har blitt lagt til i sangheftet.",
-                                status: 'success',
-                                duration: 3000,
-                                isClosable: true,
-                            })
-                        } isFullWidth colorScheme="teal">Legg til ny sang</Button>
+                        onClick={handleClick} isFullWidth colorScheme="teal">Legg til ny sang</Button>
                     <Button isFullWidth>Fullfør sanghefte</Button>
                 </VStack>
             </Container>
