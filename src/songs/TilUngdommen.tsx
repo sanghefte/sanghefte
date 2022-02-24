2583babc72528034c7e9c0076447e4f62391b749
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2022-02-24 13:33:18 +0100

    Rollback deletion of songs

---
+++ src/songs/TilUngdommen.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export function TilUngdommen() {
+	return (
+		<div className="song">
+			<h1>Til ungdommen</h1>
+			<p>
+				Kringsatt av fiender, gå inn i din tid!<br />
+				Under en blodig storm – vi dig til strid!<br />
+				Kanskje du spør i angst, udekket, åpen:<br />
+				hvad skal jeg kjempe med, hvad er mitt våpen?
+			</p>
+
+			<p>
+				Her er ditt vern mot vold, her er ditt sverd:<br />
+				troen på livet vårt, menneskets verd.<br />
+				For all vår fremtids skyld, søk det og dyrk det,<br />
+				dø om du må – men: øk det og styrk det!
+			</p>
+
+			<p>
+				Stilt går granatenes glidende bånd.<br />
+				Stans deres drift mot død, stans dem med ånd!<br />
+				Krig er forakt for liv. Fred er å skape.<br />
+				Kast dine krefter inn: døden skal tape!
+			</p>
+
+			<p>
+				Elsk – og berik med drøm – alt stort som var!<br />
+				Gå mot det ukjente, fravrist det svar.<br />
+				Ubygde kraftverker, ukjente stjerner –<br />
+				skap dem, med skånet livs dristige hjerner!
+			</p>
+
+			<p>
+				Edelt er mennesket, jorden er rik!<br />
+				Finnes her nød og sult, skyldes det svik.<br />
+				Knus det! I livets navn skal urett falle.<br />
+				Solskinn og brød og ånd eies av alle.
+			</p>
+
+			<p>
+				Da synker våpnene maktesløs ned!<br />
+				Skaper vi menneskeverd, skaper vi fred.<br />
+				Den som med høire arm bærer en byrde,<br />
+				dyr og umistelig, kan ikke myrde.
+			</p>
+
+			<p>
+				Dette er løftet vårt fra bror til bror:<br />
+				vi vil bli gode mot menskenes jord.<br />
+				Vi vil ta vare på skjønnheten, varmen –<br />
+				som om vi bar et barn varsomt på armen!
+			</p>
+		</div>
+	);
+}
