5e25e4d9d30b9b5c17bfbb6d8638ec38e6540e21
vemund <x16759109+Vemhas@users.noreply.github.com>
2022-02-24 13:50:02 +0100

    Add prettier to devDep and run "format"

--- src/util/firebase-config.ts
+++ src/util/firebase-config.ts
@@ -1,19 +1,17 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyBkn5kYiULXFNptCXoxRK2L_DlsHj_ZO_4",
+  authDomain: "sang-firebase-test.firebaseapp.com",
+  databaseURL:
+    "https://sang-firebase-test-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sang-firebase-test",
+  storageBucket: "sang-firebase-test.appspot.com",
+  messagingSenderId: "146151998654",
+  appId: "1:146151998654:web:d4295a7e63169a22a34aef",
 
-    apiKey: "AIzaSyBkn5kYiULXFNptCXoxRK2L_DlsHj_ZO_4",
-    authDomain: "sang-firebase-test.firebaseapp.com",
-    databaseURL:
-        "https://sang-firebase-test-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "sang-firebase-test",
-    storageBucket: "sang-firebase-test.appspot.com",
-    messagingSenderId: "146151998654",
-    appId: "1:146151998654:web:d4295a7e63169a22a34aef",
-
-    measurementId: "G-RBH92XFPE6"
-
+  measurementId: "G-RBH92XFPE6",
 };
 
 const app = initializeApp(firebaseConfig);
