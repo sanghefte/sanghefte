9daea01ca69ddc4083c59c1d5162aeea533ab323
trymve <trymve@stud.ntnu.no>
2022-02-28 10:57:35 +0100

    UPDATE: Sangene legges inn i riktig hefte

---
+++ src/store/store.ts
@@ -0,0 +1,9 @@
+import { atom } from "recoil"
+
+/*
+Global storage for å vite hvilket sanghefte man er inne på
+ */
+export const sanghefteState = atom({
+    key: "sanghefteState",
+    default: "test"
+})
