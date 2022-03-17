4c5e17a6b672c01519b11f25fe0ec57eb7867c5b
trymve <trymve@stud.ntnu.no>
2022-03-17 12:23:08 +0100

    Lint

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,20 @@ import { NewSong } from "./components/NewSong";
 import { LandingPage } from "./components/LandingPage";
 import { RecoilRoot } from "recoil";
 import { SongContainer } from "./components/SongContainer";
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <ChakraProvider>
       <RecoilRoot>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/" element={<LandingPage />}>
-
-                  </Route>
-                  <Route path="/newsong" element={<NewSong />}>
-
-                  </Route>
-                  <Route path="/sing" element={<SongContainer />}>
-
-                  </Route>
-
-              </Routes>
-          </BrowserRouter>,
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage/>}/>
+            <Route path="/newsong" element={<NewSong/>}/>
+            <Route path="/sing" element={<SongContainer/>}/>
+          </Routes>
+        </BrowserRouter>
+        ,
       </RecoilRoot>
     </ChakraProvider>
   );
