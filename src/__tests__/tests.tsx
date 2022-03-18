e1cca4050b0d43d08cdfed52c591c3d01a923185
trymve <trymve@stud.ntnu.no>
2022-03-18 12:15:59 +0100

    ADD: lint

--- src/__tests__/tests.tsx
+++ src/__tests__/tests.tsx
@@ -2,39 +2,38 @@
  * @jest-environment node
  */
 import {
-    checkIfPamphletExist,
-    createSanghefte,
-    createSong,
-    deleteSanghefte,
-    deleteSong,
-    getAllSongs,
+  checkIfPamphletExist,
+  createSanghefte,
+  createSong,
+  deleteSanghefte,
+  deleteSong,
+  getAllSongs,
 } from "../util/firestoreFunctions";
 
-
 test("Create and read a song-pamphlet with a song", async () => {
-    await createSanghefte("testHefte")
-    await createSong("testHefte", "title", "text", "creator")
-    const data = await getAllSongs("testHefte")
-    await expect(data[0].title).toBe("title")
-    await expect(data[0].text).toBe("text")
-    await expect(data[0].creator).toBe("creator")
-})
+  await createSanghefte("testHefte");
+  await createSong("testHefte", "title", "text", "creator");
+  const data = await getAllSongs("testHefte");
+  await expect(data[0].title).toBe("title");
+  await expect(data[0].text).toBe("text");
+  await expect(data[0].creator).toBe("creator");
+});
 
 test("Update songpamphlet", async () => {
-    await createSong("testHefte", "title", "newText", "newCreator")
-    const newData = await getAllSongs("testHefte")
-    await expect(newData[0].text).toBe("newText")
-    await expect(newData[0].creator).toBe("newCreator")
-})
+  await createSong("testHefte", "title", "newText", "newCreator");
+  const newData = await getAllSongs("testHefte");
+  await expect(newData[0].text).toBe("newText");
+  await expect(newData[0].creator).toBe("newCreator");
+});
 
 test("Delete song", async () => {
-    await deleteSong("testHefte", "title")
-    const dataAfterDelete = await getAllSongs("testHefte")
-    await expect(dataAfterDelete.length).toBe(0)
-})
+  await deleteSong("testHefte", "title");
+  const dataAfterDelete = await getAllSongs("testHefte");
+  await expect(dataAfterDelete.length).toBe(0);
+});
 
 test("Delete song-pamphlet", async () => {
-    await deleteSanghefte("testHefte")
-    const isDeleted = await checkIfPamphletExist("testHefte")
-    expect(isDeleted).toBe(false)
-})
+  await deleteSanghefte("testHefte");
+  const isDeleted = await checkIfPamphletExist("testHefte");
+  expect(isDeleted).toBe(false);
+});
