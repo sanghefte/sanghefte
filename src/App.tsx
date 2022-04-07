007cc8dcdb728e8f551d8a00dc859c16df208367
vemund <x16759109+Vemhas@users.noreply.github.com>
2022-04-07 11:34:24 +0100

    Add song details when clicking on pamphlet title

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ function App() {
             <Route path="/pamphlet" element={<NewPamphlet />} />
           </Routes>
         </BrowserRouter>
-        ,
       </RecoilRoot>
     </ChakraProvider>
   );
